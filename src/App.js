646021f5dd1e0427fa557511cd35225f135639db
DarraghKennedy1 <22346945@studentmail.ul.ie>
2025-05-26 18:17:34 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useMemo, useState, createContext, useContext } from 'react';
 import { CssBaseline, Container, Box, ThemeProvider } from '@mui/material';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
